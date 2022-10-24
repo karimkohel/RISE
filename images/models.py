c56a5fab1184e233d964e7fbf5fbbb4266aca201
karimkohel <kareemkohel@gmail.com>
2022-10-25 00:01:15 +0200

    image model

--- images/models.py
+++ images/models.py
@@ -1,3 +1,14 @@
+from distutils.command.upload import upload
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+
+    class TypeChoices(models.TextChoices):
+        DATABASE = "DATABASE", "DATABASE"
+        QUERY = "QUERY", "QUERY"
+
+
+    title = models.CharField(max_length=250)
+    type = models.CharField(max_length=50, choices=TypeChoices.choices, default=TypeChoices.QUERY)
+    image_file = models.ImageField()
