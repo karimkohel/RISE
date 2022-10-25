3921743b0106f0def8e7fa90a52a3552bb0f5aa5
karimkohel <kareemkohel@gmail.com>
2022-10-25 15:47:26 +0200

    reworked main list view into a generic class

--- images/views.py
+++ images/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from .models import Image
+from django.views.generic.list import ListView
 
 # Create your views here.
 def homeView(request):
     return render(request, 'images/index.html')
+
+
+class ImageListView(ListView):
+    model = Image
+    context_object_name = 'images'
+    paginate_by = 20
+    template_name = 'images/index.html'
+
+    # TODO: after implementing the search algorithm use it here
+    # def get_queryset(self):
+    #     return object_list
