49daf5999205f3d00f22a44e2a0e1a78c6174c30
karimkohel <kareemkohel@gmail.com>
2022-11-07 21:41:56 +0200

    teamviewr commit

---
+++ search/imageSearch.py
@@ -0,0 +1,16 @@
+import cv2
+import numpy as np
+
+img = cv2.imread('./img/about.jpg')
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+img = cv2.resize(img,(300, 300))
+
+imgFlat = np.float32(img.reshape(img.shape[0] * img.shape[1],3))
+
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+flags = cv2.KMEANS_RANDOM_CENTERS
+K=3
+
+ret, labels, centers = cv2.kmeans(imgFlat, K, None, criteria, 10, flags)
+centers = np.uint8(centers)
+print(centers)
