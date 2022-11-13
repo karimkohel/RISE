6fd3d8b770e3df4a6fa3d91aa370fddcf4e20069
karimkohel <kareemkohel@gmail.com>
2022-11-13 14:16:15 +0200

    searcher class written

---
+++ search/searcher.py
@@ -0,0 +1,34 @@
+import numpy as np
+import csv
+
+class Searcher:
+    def __init__(self, indexPath):
+		# store our index path
+        self.indexPath = indexPath
+    def search(self, queryFeatures, limit = 10):
+        # initialize our dictionary of results
+        results = {}
+        with open(self.indexPath) as f:
+            # initialize the CSV reader
+            reader = csv.reader(f)
+            # loop over the rows in the index
+            for row in reader:
+                # parse out the image ID and features, then compute the
+                # chi-squared distance between the features in our index
+                features = [float(x) for x in row[1:]]
+                d = self.chi2_distance(features, queryFeatures)
+
+                results[row[0]] = d
+            # close the reader
+            f.close()
+        # sort our results, so that the smaller distances (i.e. the
+        # more relevant images are at the front of the list)
+        results = sorted([(v, k) for (k, v) in results.items()])
+
+        return results[:limit]
+
+
+    def chi2_distance(self, histA, histB, eps = 1e-10):
+		# compute the chi-squared distance
+        d = 0.5 * np.sum([((a - b) ** 2) / (a + b + eps) for (a, b) in zip(histA, histB)])
+        return d
